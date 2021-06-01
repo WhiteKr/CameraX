894742980cd3715c1b01b6b151083d960ede4527
WhiteKr <hanjisu2215g@gmail.com>
2021-06-01 14:01:32 +0900

    Android project build

---
+++ app/src/main/java/kr/hs/dukyoung/camerax/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.hs.dukyoung.camerax
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
